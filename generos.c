188a89b49a7d836367b26a1b36d7e399f6efe606
Duda Kuhn <meduardakuhn@gmail.com>
2026-07-30 14:33:55 -0300

    criar funções para os generos de musica

--- generos.c
+++ generos.c
@@ -2,3 +2,106 @@
 #include <stdlib.h>
 #include <string.h>
 #include "generos.h"
+
+// Cria a lista de generos
+void inicializarLista(ListaGeneros *l){
+    l->ini = NULL;
+}
+
+// Insere um genero novo na lista
+void inserirGenero(ListaGeneros *l, char nome[]){
+    Genero *novo = (Genero*) malloc(sizeof(Genero));
+    if(novo == NULL){
+        printf("Erro ao inserir Genero\n");
+        return;
+    }
+    strcpy(novo->nome, nome);
+    novo->listaArtistas = NULL;
+    novo->prox = l->ini;
+    novo->ant = NULL;
+    
+    if(l->ini != NULL){
+        l->ini->ant = novo;
+    }
+    l->ini = novo;
+}
+
+Genero* buscarGenero(ListaGeneros *l, char nome[]){
+    Genero *atual = l->ini;
+    while(atual!= NULL){
+        if(strcmp(atual->nome, nome) == 0){
+            return atual;
+        }
+        atual = atual->prox;
+    }
+    return NULL;
+}
+
+
+void alterarGenero(ListaGeneros *l, char nomeAntigo[], char nomeNovo[]){
+    Genero *atual = buscarGenero(l, nomeAntigo);
+    if(atual == NULL){
+        printf("Genero nao encontrado\n");
+        return;
+    }
+    strcpy(atual->nome, nomeNovo);
+    printf("Nome alterado com sucesso\n");
+
+}
+void removerGenero(ListaGeneros *l, char nome[]){
+    Genero *atual = buscarGenero(l, nome);
+    if(atual == NULL){
+        printf("Genero nao encontrado\n");
+        return;
+    }
+    atual->prox->ant = atual->ant;
+    atual->ant->prox = atual->prox;
+    //LEMBRAR FREE
+
+
+}
+
+void listarGeneros(ListaGeneros *l){
+    Genero *atual = l->ini;
+    printf("Lista de generos: ");
+    while(atual->prox!=NULL){
+        printf("%s - ", atual->nome);
+        atual = atual ->prox;
+    }
+    if(atual != NULL){
+        printf("%s\n", atual->nome);
+    }
+}
+
+
+int contarGeneros(ListaGeneros *l){
+    int contador = 0;
+    Genero *atual = l->ini;
+    while(atual != NULL){
+        contador++;
+        atual = atual->prox;
+    }
+    return contador;
+}
+
+void destruirLista(ListaGeneros *l){
+    if(l == NULL){
+        return;
+    }
+    Genero *atual = l->ini;
+    Genero *prox;
+    while(atual != NULL){
+        prox = atual->prox;
+        free(atual);
+        atual = prox;
+    }
+    l->ini = NULL;
+    //DESTRUIR LISTA DE DENTRO AAAAAAA
+}
+
+//  Funcões da segunda lista
+void inserirArtista(ListaGeneros *l, char nomeGenero[]){
+    Genero *atual = buscaGenero(l, nomeGenero);
+    
+}
+
--- generos.h
+++ generos.h
@@ -1,12 +1,52 @@
+#include <stdbool.h>
 #ifndef _GENEROS_H
 #define _GENEROS_H
 
+// Lista de Músicas
+typedef struct mus {
+    int ano;
+    char nome[40];
+    char album[40];
+    struct mus *prox;
+} Musica;
+
+// Lista de Artistas
+typedef struct no1 {
+    char nome[30];
+    char cidadeOrigem[30];
+    int anosAtuacao;
+    bool aindaAtua;
+    int premiacoes;
+    char integrantes[200];
+    Musica *listaMusicas; // Ponteiro para a lista encadeada de músicas
+    struct no1 *prox;
+} Artista;
+
+// Lista Duplamente Encadeada de Gêneros 
+typedef struct no {
+    char nome[15];
+    struct no *ant;
+    struct no *prox;
+    Artista *listaArtistas; // Ponteiro para a lista encadeada de artistas
+} Genero;
+
+// No Descritor Para Gêneros
+typedef struct lista{
+    Genero *ini;
+} ListaGeneros;
 
 
+// Funções Gêneros 
+void inicializarLista(ListaGeneros *l);
+void inserirGenero(ListaGeneros *l, char nome[]);
+Genero *buscarGenero(ListaGeneros *l, char nome[]);
+void alterarGenero(ListaGeneros *l, char nomeAntigo[], char nomeNovo[]);
+void removerGenero(ListaGeneros *l, char nome[]);
+void listarGeneros(ListaGeneros *l);
+int contarGeneros(ListaGeneros *l);
+void destruirLista(ListaGeneros *l);
 
+//Funções Artistas
+void inserirArtista()
 
-
-
-
-
-#endif 
+#endif
